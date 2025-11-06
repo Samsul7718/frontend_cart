1e8d7739d28f604451a45d65ae788c161184f621
samsul alam <samsul7718@gmail.com>
2025-11-06 19:45:59 +0530

    fetch backend url inside useEffect

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useState } from 'react'
 
 const HomePage = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:3000/api/products")
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+        .catch(err=>console.log(err))
+    })
   return (
     <div>HomePage</div>
   )
