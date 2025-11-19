b64a9ac2c7648c93c4f4c8dae91be18f628b0fc7
samsul alam <samsul7718@gmail.com>
2025-11-19 22:38:25 +0530

    working on payment page

--- src/PaymentPage.jsx
+++ src/PaymentPage.jsx
@@ -6,6 +6,9 @@ const PaymentPage = () => {
       <h3>PaymentPage</h3>
 
         <p>payment successfully completed</p>
+        <p>get ready for order
+        delivery...
+        </p>
     </div>
   )
 }
